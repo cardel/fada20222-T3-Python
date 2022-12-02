ec30f4e91b02f7e25b610c1eb853ac35db7d8b5a
Carlos Delgado <cardel87@gmail.com>
2022-12-02 16:28:38 -0500

    tests

--- test_arbolesRojinegros.py
+++ test_arbolesRojinegros.py
@@ -1,6 +1,7 @@
 """
 Pruebas de la clse de ArbolRojinegro
 """
+import math
 import pytest
 
 from ArbolesRojinegros import ArbolRojinegro
@@ -11,6 +12,19 @@ from data.RojinegroGenerator import RojinegroGenerador
 def gen_arboles():
     gen = RojinegroGenerador()
     return gen.ejemplo1(), gen.ejemplo2(), gen.ejemplo3(), gen.ejemplo4()
+
+    
+    
+def altura(arb):
+        alturaIzq = 0
+        alturaDer = 0
+        if arb.izq is not None:
+            alturaIzq = 1 + altura(arb.izq)
+
+        if arb.der is not None:
+            alturaDer = 1 + altura(arb.der)
+
+        return alturaIzq if (alturaIzq > alturaDer)  else alturaDer
 
 
 def test_recorridos(gen_arboles):
@@ -29,23 +43,24 @@ def test_recorridos(gen_arboles):
 
 
 def test_insertar():
-    num = [8, 7, 6, 12, 10, 9, 11, 14, 15, 13]
-    instancia = ArbolRojinegro(None, None, None, None)
+    num = [7, 6, 12, 10, 9, 11, 14, 15, 13, -3, 44, 66, 72, -10, -13]
+    instancia = ArbolRojinegro(None, None, 8, True)
 
     for n in num:
         instancia.insertar(n)
 
-    assert instancia.bfs(), "10 7 12 6 8 11 14 9 13 15"
-    assert instancia.inorden(), "6 7 8 9 10 11 12 13 14 15"
+    assert instancia.inorden(), "-13 -10 -3 6 7 8 9 10 11 12 13 14 15 44 66 72"
+    assert altura(instancia) <= 2 * math.log2(15) + 1
 
-    numB = [20, 22, 1, 2, 3, 9, 14, 17, 0, 33, 7, 13, 19]
-    instanciaB = ArbolRojinegro(None, None, None, None)
+    numB = [22, 1, 2, 3, 9, 14, 17, 0, 33, 7, 13, 19, -1, -7, -9, 50, -22, 66, -14, -32]
+    instanciaB = ArbolRojinegro(None, None, 20, True)
 
     for n in numB:
         instanciaB.insertar(n)
 
-    assert instanciaB.bfs(), "9 2 20 1 3 14 22 0 7 13 17 33 19"
-    assert instanciaB.inorden(), "0 1 2 3 7 9 13 14 17 19 20 22 33"
+    assert altura(instancia) <= 2 * math.log2(20) + 1
+
+    assert instanciaB.inorden(), "-32 -22 -14 -9 -7 -1 0 1 2 3 7 9 13 14 17 19 20 22 33 50 66"
 
 
 def test_maximo(gen_arboles):
@@ -68,10 +83,10 @@ def test_minimo(gen_arboles):
 def test_search(gen_arboles):
     arb1, arb2, arb3, arb4 = gen_arboles
 
-    assert arb1.search(5).getValor() == 5
-    assert arb2.search(6).getValor() == 6
-    assert arb3.search(3).getValor() == 3
-    assert arb4.search(9).getValor() == 9
+    assert arb1.search(5).value == 5
+    assert arb2.search(6).value == 6
+    assert arb3.search(3).value == 3
+    assert arb4.search(9).value == 9
 
 
 def test_rotacionIzquierda(gen_arboles):
@@ -100,7 +115,7 @@ def test_rotacionDerecha(gen_arboles):
     assert arb3.bfs() == "2 1 4 3 5"
 
     # Execute
-    arb4.rotacionIzquierda(5)
+    arb4.rotacionDerecha(5)
 
     # Assert
     assert arb4.bfs() == "5 1 8 6 9"
